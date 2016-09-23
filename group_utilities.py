d9b8ae3835b3d0239f5ab630816af219926387b3
Stephan Cilliers <stephanus.cilliers@gmail.com>
2016-09-23 15:52:19 +0200

    Removed bot command

--- group_utilities.py
+++
@@ -1,9 +0,0 @@
-def read_lines(filename):
-    lines = []
-    try:
-        with open(filename, 'r') as f:
-            for line in f:
-                lines.append(line[:-1])
-    except IOError:
-        print 'Failed to open {}'.format(filename)
-    return lines
